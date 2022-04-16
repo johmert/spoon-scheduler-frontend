75f3665fc3d6cecdb1566b1492d0ed0283c4e079
johmert <johmert@users.noreply.github.com>
2022-04-15 20:43:49 -0700

    additonal styling

--- src/layout/UserForm.js
+++ src/layout/UserForm.js
@@ -3,6 +3,7 @@ import { createUser, loginUser } from "../utils/api/index"
 import Button from "react-bootstrap/Button"
 import Container from "react-bootstrap/Container"
 import Form from "react-bootstrap/Form"
+import Stack from "react-bootstrap/Stack"
 
 function UserForm({setToken}) {
     const [mode, setMode] = useState("login");
@@ -13,20 +14,20 @@ function UserForm({setToken}) {
     const label = mode === "login" ? "Login" : "Register";
 
     const loginForm = (
-        <div>
-            <Button variant="primary" type="submit">Submit</Button>
-            <span className="mx-3">Not a user? </span>
-            <Button variant="outline-primary" onClick={clickRegister}>Register Now!</Button>        
-        </div>
+        <Stack className="d-flex justify-content-center">
+            <Button className="mb-3" variant="primary" type="submit">Submit</Button>
+            <p className="my-2 align-self-center">Not a user? </p>
+            <Button variant="outline-primary" onClick={clickRegister}>Register Now!</Button> 
+        </Stack>
     );
 
     const registerForm = (<>
         <Form.Label>Confirm Password: </Form.Label>
         <Form.Control className="mb-3" type="password" onChange={e => setConfirmPassword(e.target.value)} />                
-        <div>
-            <Button variant="primary" type="submit">Submit</Button>
+        <Stack className="d-flex justify-content-center">
+            <Button className="mb-3" variant="primary" type="submit">Submit</Button>
             <Button variant="outline-primary" onClick={()=> window.location.reload(false)}>Cancel</Button>
-        </div>
+        </Stack>
     </>);
 
     const [form, setForm] = useState(loginForm);
@@ -68,7 +69,7 @@ function UserForm({setToken}) {
 
     return(
         <Container>
-            <span className="h1">Please {label}</span>
+            <h1 className="text-center">Please {label}</h1>
             <Form className="container" onSubmit={handleSubmit}>
                 <Form.Label>Username: </Form.Label>
                 <Form.Control className="mb-3" type="text" onChange={e => setUsername(e.target.value)} />                
