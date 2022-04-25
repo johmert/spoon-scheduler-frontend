0a3af9010671b6e078badc842a55d89c7cad05b3
johmert <johmert@users.noreply.github.com>
2022-04-25 00:28:42 -0700

    deploy commit

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:8080";
+const API_BASE_URL = "https://spoon-scheduler-backend.herokuapp.com";
 
 const headers = new Headers();
 headers.append("Content-Type", "application/json");
